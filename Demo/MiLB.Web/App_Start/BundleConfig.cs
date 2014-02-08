a0386b9b9309e836047e40d1d007b93bb63a56e7
Nik Molnar <nikmd23@gmail.com>
2014-02-07 22:23:28 -0500

    Setup bundling + minification demo

--- Demo/MiLB.Web/App_Start/BundleConfig.cs
+++ Demo/MiLB.Web/App_Start/BundleConfig.cs
@@ -7,6 +7,8 @@ namespace MiLB.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new StyleBundle("~/Content/css")
                 .Include("~/Content/normalize.css")
                 .Include("~/Content/validation.css")
