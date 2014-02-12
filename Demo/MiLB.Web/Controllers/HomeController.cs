9bc2cfc95c46f5508cb42a76e1d2bdfb407428f9
Nik Molnar <nikmd23@gmail.com>
2014-02-12 09:58:05 -0500

    Prerender demo + script in slides

--- Demo/MiLB.Web/Controllers/HomeController.cs
+++ Demo/MiLB.Web/Controllers/HomeController.cs
@@ -38,6 +38,13 @@ namespace MiLB.Web.Controllers
             Response.Write("console.log('Downloaded 3rd party script.'); function init(){ console.log('Initialized 3rd party script.'); }");
         }
 
+        public ActionResult Champions()
+        {
+            var mascots = dataContext.Mascots.Where(m => m.IsChampion).ToList();
+
+            return View(mascots);
+        }
+
         protected override void Dispose(bool disposing)
         {
             dataContext.Dispose();
