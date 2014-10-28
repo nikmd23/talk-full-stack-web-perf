70e461ae51adcc237c1615c5cb6f8fe8fa326cbf
Nik Molnar <nikmd23@gmail.com>
2014-10-28 11:55:24 -0400

    Sprite all leagues

--- Demo/MiLB.Web/App_Start/BundleConfig.cs
+++ Demo/MiLB.Web/App_Start/BundleConfig.cs
@@ -13,7 +13,19 @@ namespace MiLB.Web
                 .Include("~/Content/normalize.css")
                 //.Include("~/Content/validation.css")
                 .Include("~/Content/screen.css")
-                .Include("~/Content/Images/MascotSmall/texas/Texas.png.css"));
+                .Include("~/Content/Images/MascotSmall/texas/Texas.css")
+                .Include("~/Content/Images/MascotSmall/Southern/Southern.css")
+                .Include("~/Content/Images/MascotSmall/South-Atlantic/South-Atlantic.css")
+                .Include("~/Content/Images/MascotSmall/Pioneer/Pioneer.css")
+                .Include("~/Content/Images/MascotSmall/northwest/Northwest.css")
+                .Include("~/Content/Images/MascotSmall/New-York-Penn/New-York-Penn.css")
+                .Include("~/Content/Images/MascotSmall/Midwest/Midwest.css")
+                .Include("~/Content/Images/MascotSmall/International/International.css")
+                .Include("~/Content/Images/MascotSmall/Florida-State/Florida-State.css")
+                .Include("~/Content/Images/MascotSmall/Eastern/Eastern.css")
+                .Include("~/Content/Images/MascotSmall/Carolina/Carolina.css")
+                .Include("~/Content/Images/MascotSmall/California/California.css")
+                .Include("~/Content/Images/MascotSmall/Appalachian/Appalachian.css"));
 
             bundles.Add(new ScriptBundle("~/Scripts/js")
                 .Include("~/Scripts/jquery-{version}.js")
